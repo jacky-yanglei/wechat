c6d0f818f9212b94234425fcb442e248427d6742
jacky-yanglei <651300929@qq.com>
2021-12-29 14:38:27 +0800

    1

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -13,7 +13,7 @@ export default {
     mounted() {
       axios.post(process.env.VUE_APP_BASE_URL + 'script/check_register/' + this.$route.params.id).then(({data}) => {
         if (data.data) {
-          this.$router.push('/login')
+          this.$router.push('/dmlogin')
         } else {
           this.$router.push('/register/' + this.$route.params.id)
         }
