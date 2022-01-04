cebe4dbba6127a8e835c779026f70f74a1e278b9
jacky-yanglei <651300929@qq.com>
2022-01-04 16:35:50 +0800

    1

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -626,13 +626,16 @@ export default {
         padding: 0 16px;
         margin-bottom: 10px;
         font-size: 16px;
-        > div {
-            border: 2px solid #A89061;
-            padding: 0 5px;
-            border-radius: 4px;
-        }
+        // > div {
+        //     border: 2px solid #A89061;
+        //     padding: 0 5px;
+        //     border-radius: 4px;
+        // }
     }
     .exit-room {
+        border: 2px solid #A89061;
+        padding: 0 5px;
+        border-radius: 4px;
         cursor: pointer;
         color: #bc634f;
     }
